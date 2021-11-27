82ca4dfd1459b1453299370edbc844a4884816de
Kelmend Imeri <kelmendimeri@gmail.com>
2021-11-27 18:54:03 +0100

    homework 6

--- homework/class06/middlewares/residentValidator.js
+++ homework/class06/middlewares/residentValidator.js
@@ -1,11 +1,11 @@
 const validateBody = async (req, res, next) => {
-    const body = req.body;
+  const body = req.body;
   
-    if (!body.name || !body.hasCar || !body.isBoomer) {
-      return res.status(400).json('Missing required field!');
-    }
-  
-    return next();
-  };
+  if (body.name === undefined || body.hasCar === undefined || body.isBoomer=== undefined) {
+    return res.status(400).json('Missing required field!');
+  }
+
+  return next();  
+};
   
   module.exports = validateBody;
--- homework/class06/routes/index.js
+++ homework/class06/routes/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const controller = require('../controller/residents');
+const bodyValidator = require('../middlewares/residentValidator');
 
 router.get("/residents", controller.getAllResidents);
-router.post("/residents", controller.addNewResident);
-router.put("/residents/:name", controller.updateResident);
+router.post("/residents", bodyValidator, controller.addNewResident);
+router.put("/residents/:name", bodyValidator, controller.updateResident);
 router.delete("/residents/:name", controller.deleteResident);
 
 module.exports = router;
